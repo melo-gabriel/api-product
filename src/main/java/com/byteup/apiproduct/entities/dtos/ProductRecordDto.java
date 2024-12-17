56f955b061e2b64507228b271edd7bbab4bfb6ab
gabriel <silva.gabrilmelosantos@gmail.com>
2024-12-16 23:19:44 -0300

    api cache redis

--- src/main/java/com/byteup/apiproduct/entities/dtos/ProductRecordDto.java
+++ src/main/java/com/byteup/apiproduct/entities/dtos/ProductRecordDto.java
@@ -1,7 +1,8 @@
 package com.byteup.apiproduct.entities.dtos;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public record ProductRecordDto(String name, Date expirationDate, Float price, int quantity) {
+public record ProductRecordDto(String name, Date expirationDate, Float price, int quantity) implements Serializable {
 }
 
--- src/main/java/com/byteup/apiproduct/services/ProductService.java
+++ src/main/java/com/byteup/apiproduct/services/ProductService.java
@@ -6,18 +6,25 @@ import com.byteup.apiproduct.entities.ProductEntity;
 import com.byteup.apiproduct.repositories.ProductRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Service;
 
 @Service
+@EnableCaching
 public class ProductService {
 
     @Autowired
     public ProductRepository productRepository;
 
+
+    @Cacheable(value = "products",  key = "#id")
     public ProductEntity getById(Long id) {
         return productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException("Product not found!"));
     }
 
+    @CacheEvict(value = "products", key = "#products.id")
     public ProductEntity updateProduct(Long id, ProductRecordDto productRecordDto) {
         ProductEntity productModel = productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException("Product not found!"));
         BeanUtils.copyProperties(productRecordDto, productModel);
@@ -29,4 +36,6 @@ public class ProductService {
         BeanUtils.copyProperties(productRecordDto, productModel);
         return productRepository.save(productModel);
     }
+
+ //   docker run --name my-redis -p 6379:6379 -d redis
 }
